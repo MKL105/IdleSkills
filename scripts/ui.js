33ecc942a3a83756f480eedb37d3bd34d08b1bc9
MKL105 <markus99ml@gmail.com>
2023-09-18 14:12:30 +0200

    Set up basic skills layout

---
+++ scripts/ui.js
@@ -0,0 +1,66 @@
+//Views
+const gameView = document.getElementById('game-view');
+const settingsView = document.getElementById('settings-view');
+const achievementsView = document.getElementById('achievements-view');
+const statsView = document.getElementById('stats-view');
+
+//Inventory variables
+const toggleInvButton = document.getElementById('toggle-inventory-button');
+const invBody = document.getElementById('inventory-body');
+
+//Initializes the UI once the window is fully loaded.
+window.onload = setupUI();
+function setupUI() {
+    //Set up views
+    gameView.style.display = 'block';
+    settingsView.style.display = 'none';
+    achievementsView.style.display = 'none';
+    statsView.style.display = 'none';
+    //Set up inventory
+    invBody.style.display = 'block';
+    toggleInvButton.textContent = 'hide';
+    //Set up skills tabs
+    document.getElementById("default-open").click();
+}
+
+//Displays and hides the inventory if the according button is clicked.
+function toggleInv() {
+    if (invBody.style.display === 'none') {
+        invBody.style.display = 'block';
+        toggleInvButton.textContent = 'hide';
+    }
+    else {
+        invBody.style.display = 'none';
+        toggleInvButton.textContent = 'show';
+    }
+}
+
+function openSettings() {
+    gameView.style.display = 'none';
+    achievementsView.style.display = 'none';
+    statsView.style.display = 'none';
+    settingsView.style.display = 'block';
+}
+function closeSettings() {
+    gameView.style.display = 'block';
+    achievementsView.style.display = 'none';
+    statsView.style.display = 'none';
+    settingsView.style.display = 'none';
+}
+
+function openSkill(event, skillName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("skills-tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("skills-tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(skillName).style.display = "block";
+    event.currentTarget.className += " active";
+}
