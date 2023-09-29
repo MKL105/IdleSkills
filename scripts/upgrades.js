912053f5dac9c266cc38d494048f5fe9460bb503
MKL105 <markus99ml@gmail.com>
2023-09-29 13:21:06 +0200

    Updated license in files

--- scripts/upgrades.js
+++ scripts/upgrades.js
@@ -1,3 +1,17 @@
+/* Copyright (C) [2023] [Markus Luethje <mklgamesapps@gmail.com>]
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the 
+ * GNU General Public License as published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <https://www.gnu.org/licenses>.
+ */
+
 const pebbleInvMaxProgression = [10, 25, 50, 100, 200, 400, 750, 1000, 1500, 2500, 5000, 7500, 10000];
 const squarePebbleInvMaxProgression = [3, 5, 10, 20, 35, 60, 100]; 
 const stickInvMaxProgression = pebbleInvMaxProgression;
